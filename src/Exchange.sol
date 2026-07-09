a14e6fe72b8a5748075dea6af02bc6589d63f0c4
satoshi0xe <emmanuelkacou225@outlook.com>
2026-07-09 05:24:25 +0000

    feat: add condition to check liquidity pool state and update swaps ratio

--- src/Exchange.sol
+++ src/Exchange.sol
@@ -13,10 +13,23 @@ contract Exchange {
     }
 
     function addLiquidity(uint256 _tokenAmount) public payable {
-        IERC20 token = IERC20(tokenAddress);
-        bool success = token.transferFrom(msg.sender, address(this), _tokenAmount);
+        if (getReserve() == 0) {
+            IERC20 token = IERC20(tokenAddress);
+            bool success = token.transferFrom(msg.sender, address(this), _tokenAmount);
 
-        require(success, "failed to add liquidity");
+            require(success, "failed to add liquidity");
+        } else {
+            uint256 ethReserve = address(this).balance - msg.value;
+            uint256 tokenReserve = getReserve();
+
+            uint256 tokenAmount = (msg.value * tokenReserve) / ethReserve; // Ratio
+
+            require(tokenAmount >= _tokenAmount, "insufficient token amount");
+
+            IERC20 token = IERC20(tokenAddress);
+
+            token.transferFrom(msg.sender, address(this), tokenAmount);
+        }
     }
 
     function getReserve() public view returns (uint256) {
